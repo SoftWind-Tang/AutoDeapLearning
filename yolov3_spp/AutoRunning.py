f963d0a3e0daeb9d4983486874f4cd9199c9dfc6
SoftWind_Tang <1216132456@qq.com>
2021-08-18 22:00:01 +0800

    update

--- yolov3_spp/AutoRunning.py
+++ yolov3_spp/AutoRunning.py
@@ -1,6 +1,6 @@
 import os
 
 for i in range(3):
+    os.system("python calculate_dataset.py")
     os.system("python train.py")
     os.system("python predict_test.py")
-    os.system("python calculate_dataset.py")
